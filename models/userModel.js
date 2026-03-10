3bff3f1ea7f3d3d0160276a301cb5fcd0ab74a35
CSwan300 <campbellswan8@gmail.com>
2026-03-10 16:57:19 +0000

    fix/chore - add instructors page and remove login by default aswell as update css

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,17 @@
-
 // models/userModel.js
 import { usersDb } from './_db.js';
 
 export const UserModel = {
-  async create(user) {
-    return usersDb.insert(user);
-  },
-  async findByEmail(email) {
-    return usersDb.findOne({ email });
-  },
-  async findById(id) {
-    return usersDb.findOne({ _id: id });
-  }
+    async create(user) {
+        return usersDb.insert(user);
+    },
+    async findByEmail(email) {
+        return usersDb.findOne({ email });
+    },
+    async findById(id) {
+        return usersDb.findOne({ _id: id });
+    },
+    async list() {
+        return usersDb.find({});
+    }
 };
-``
