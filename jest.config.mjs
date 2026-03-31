ef7a5e6bc43538335018184540301a96521ecc13
Campbell Swan <147725350+CSwan300@users.noreply.github.com>
2026-03-31 18:35:46 +0100

    Update jest.config.mjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,6 @@
 export default {
     testEnvironment: "node",
     transform: {},
-    // REMOVED: extensionsToTreatAsEsm: ['.js'], <-- This was the cause of the error
     moduleNameMapper: {
         // Allows Jest to resolve imports that include the .js extension
         '^(\\.{1,2}/.*)\\.js$': '$1',
